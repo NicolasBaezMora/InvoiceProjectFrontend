4c19c5c7602021c462f5aba0a1d32aa55d417d8f
NICOLAS BAEZ MORA <1005340687@bolivar.com>
2022-11-17 09:29:15 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Taller_17_TC_front';
+}
